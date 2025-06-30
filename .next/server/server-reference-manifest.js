959a22773e130c0b4ab8d2a6dd31d7f0ffd60d5e
Honore Kayumba <84237371+HKayumba@users.noreply.github.com>
2025-06-30 22:24:48 +0200

    Added .next to .gitignore

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"rFxsrTBL0YgRHKUe/tubk+ACLrUyxEx0dnJwnYjkLjw=\"\n}"
--- .next/static/chunks/app_layout_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__9d126714._.css",
-  "static/chunks/node_modules_d0ec30de._.js",
-  "static/chunks/_6ab2d6b5._.js"
-],
-    source: "dynamic"
-});
